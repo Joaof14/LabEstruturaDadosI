fda040b045d9e9a5d528aa41b7f32d95476862ae
Joaof14 <joao-pedro032011@hotmail.com>
2025-04-29 21:30:10 -0300

    ''

---
+++ un01/lab01/q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    //Declaração de variável
+    int numero = 0;
+
+    // Printar
+    printf("número: %d\n", numero);
+    printf("endereço: %p\n", &numero);
+
+    return 0;
+}
