70248447753886289bdb94f913b300dd95cb41ee
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-03 17:05:26 -0300

    ""

--- un01/lab01/q8.c
+++ un01/lab01/q8.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 /* Assumindo que o endereço da variável x foi atribuído a um ponteiro px,
-escreva uma expressão que não usa x e divida x por 3.Assumindo que o endereço da variável x foi atribuído a um ponteiro px,
 escreva uma expressão que não usa x e divida x por 3. */
 
 int main(){
+
+    int x = 3;
+    int * px = &x;
+
+    int resultado = (*px)/3 ; // expressão
+
+    printf("Resultado: %d\n", resultado);
+
     return 0;
 }
