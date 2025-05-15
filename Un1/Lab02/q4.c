7bb0b3aec2c79ee8e1506e5b973f126d76bc3fd4
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-15 14:53:17 -0300

    ""

--- Un1/Lab02/q4.c
+++ Un1/Lab02/q4.c
@@ -5,6 +5,26 @@ unidade. A função Mais deve receber um número inteiro através de um
 ponteiro. A função Incrementa deve receber um valor inteiro, sem usar 
 ponteiros, e retornar o valor incrementado em uma unidade. */
 
+void Mais(int * n1){
+    *n1 = *n1 + 1;
+}
+
+
+int Incrementa(int n1){
+    n1 = n1 + 1;
+    return n1;
+}
+
 int main() {
+    int n;
+    printf("Digite um valor:\n");
+    scanf(" %d", &n);
+
+    Mais(&n);
+    printf("Resultado após Mais: %d \n", n);
+
+    n = Incrementa(n);
+    printf("Resultado após Incrementa: %d \n", n);
+
     return 0;
 }
