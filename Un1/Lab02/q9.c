471f9a2e07c647a141063bf1a9e527434f308237
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-15 16:57:22 -0300

    ""

--- Un1/Lab02/q9.c
+++ Un1/Lab02/q9.c
@@ -8,6 +8,49 @@ horário atual e guarde-o na variável. Usando o ponteiro, incremente o horário
 recebido em uma hora e em seguida mostre o horário corrigido com 
 MostrarHorario.*/
 
+typedef struct 
+{
+    int horas;
+    int minutos;
+} Horario;
+
+
+void MostrarHorario(Horario * hr){
+
+    printf("Seu relógio está atrasado, o horário correto é: %d:%d\n", hr->horas, hr->minutos);
+
+}
+
 int main() {
+    Horario horaAtual;
+    Horario * ptr = &horaAtual;
+    printf("Digite a hora atual, aperte enter, e em seguida digite os minutos: \n");
+    
+    //Receber horas
+    while (1) {
+        printf("Que horas são? \n");
+        scanf("%d:%d", &horaAtual.horas, &horaAtual.minutos);
+
+        //Verificar se horario está nos requisitos
+        if( (horaAtual.horas < 24 &&  horaAtual.horas >= 00 ) && (horaAtual.minutos < 60 && horaAtual.minutos >= 00) ) {
+            break;
+        }
+        else {
+            printf("O valor em horas deve estar ser de 0 a 23 e de 0 a 59 para minutos\n");
+        }
+
+    }
+
+    //Corrigir horário
+    if (ptr->horas == 23) {
+        ptr->horas = 00;
+    }
+    else {
+        ptr->horas = ptr->horas+1;
+    }
+
+    MostrarHorario(ptr);
+    
+    
     return 0;
 }
