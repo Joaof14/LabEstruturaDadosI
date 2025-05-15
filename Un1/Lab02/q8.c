f54e53152a7622dc2815f968e58aaa134913e1c6
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-15 20:31:15 -0300

    ""

--- Un1/Lab02/q8.c
+++ Un1/Lab02/q8.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <string.h>
 
 /*  Declare um registro "Tigela" com os campos estado (cheia ou vazia) e tipo de 
 alimento (sopa ou canja). Crie uma função "Fome" que recebe um ponteiro para 
@@ -7,12 +8,27 @@ tigela cheia, crie um ponteiro que aponta para essa tigela e então mostre como 
 tigela estava antes da janta. Depois chame a função Fome com o ponteiro que 
 aponta para a tigela e ao fim mostre a tigela depois da janta.*/
 
+
+
 typedef struct {
     char estado[20];
-    char alimento[20];
+    char tipo_de_alimento[20];
+
 } Tigela;
 
+void Fome(Tigela * ptr) {
+    //ptr->estado = "vazia";
+    strcpy(ptr->estado, "vazia");
+
+}
 
 int main() {
+    Tigela bowl = {"cheia", "sopa"};
+    Tigela * p = &bowl;
+
+    printf("A Tigela antes da janta estava %s de %s\n", p->estado, p->tipo_de_alimento);
+    Fome(p);
+    printf("A Tigela depois da janta estava %s\n", p->estado);
+
     return 0;
 }
