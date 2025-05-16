5d446ba6bccdd6d6d0845d2a91f79ea344f7bcdc
Joaof14 <joao-pedro032011@hotmail.com>
2025-05-15 22:05:43 -0300

    ""

--- Un1/Lab02/q7.c
+++ Un1/Lab02/q7.c
@@ -12,26 +12,48 @@ modificar a variável recebida, sem retornar valor.*/
 
 
 typedef struct {
-    int8_t R;
-    int8_t G;
-    int8_t B;
-    int8_t A;
+    int R;
+    int G;
+    int B;
+    int A;
 } rgba;
 
 union Cor {
     rgba corRGBA;
-    int32_t cor32;
+    unsigned long int cor32;
 
 };
 
-void RGBA() {
+void ReceberRGBA(union Cor *cor) {
+    while (1) {
+        printf("Insira valores de cores RGBA (0-255) separados por espaço\n");
+        scanf("%d %d %d %d", &cor->corRGBA.R, &cor->corRGBA.G, &cor->corRGBA.B, &cor->corRGBA.A);
+        if ( (cor->corRGBA.R <= 255 && cor->corRGBA.R >= 0 ) && (cor->corRGBA.G <= 255 && cor->corRGBA.G >= 0 ) && (cor->corRGBA.B <= 255 && cor->corRGBA.B >= 0 ) && (cor->corRGBA.A <= 255 && cor->corRGBA.A >= 0 )  )
+        {break;}
+    }
+
 
 }
 
-void Int32(){
+void ReceberInt32(union Cor *cor){
+    //int max = 4294967295;
+    while (1) {
+        printf("Digite um valor de 0 a 4294967295\n");
+        scanf("%lu", &cor->cor32);
+
+        if(cor->cor32 <= 4294967295 && cor->cor32 >= 0) {
+            break;
+        }
+    }
 
 }
 
 int main() {
+    union Cor cor;
+
+    ReceberRGBA(&cor);
+    printf("\n");
+    ReceberInt32(&cor);
+    
     return 0;
 }
