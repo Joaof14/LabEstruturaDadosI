255f8f720c7b2b8e466873cfcb7e522d53435614
Joaof14 <joao-pedro032011@hotmail.com>
2025-06-07 15:34:08 -0300

    "q3"

--- Un1/Lab04/q3.c
+++ Un1/Lab04/q3.c
@@ -2,3 +2,24 @@
 arquivo texto, chamado “pescado.txt”, e exiba a quantidade total de quilos de
 peixe pescado. O arquivo é formatado como no exemplo abaixo: o nome do
 peixe, o peso em gramas e o comprimento em centímetros.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    FILE * arquivo = fopen("arquivos/pescados.txt", "r");
+    
+    float soma = 0;
+    char nome[100];
+    int peso, tamanho;
+    if (arquivo == NULL )
+        {printf("Erro ao abrir arquivo");}
+
+    while (fscanf(arquivo, "%99s %d %d", nome, &peso, &tamanho) == 3)
+    {soma += (peso / 1000.0f);}
+    
+    printf("%f kg de peixe pescados!", soma);
+    fclose(arquivo);
+    return 1;
+}
