b74fbcd1cd134e42e624f79d48b6a419d8f95b87
Joaof14 <joao-pedro032011@hotmail.com>
2025-06-07 14:10:59 -0300

    "questao 1"

--- Un1/Lab04/q1.c
+++ Un1/Lab04/q1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /* 
 Considerando o arquivo texto apresentado, o que o trecho de código abaixo
 faz?
@@ -7,7 +9,7 @@ faz?
 int main()
 {
 FILE *fin;
-fin = fopen("intro.txt", "r");
+fin = fopen("arquivos/intro.txt", "r");
 char ch;
 fscanf(fin, "%c", &ch);
 fclose(fin);
@@ -21,3 +23,76 @@ b) Uma palavra
 c) A quinta palavra
 d) Uma linha
 */
+
+
+
+
+
+
+void a(){
+    printf("\n\nLetra a) O oitavo caractere \n");
+
+    FILE *fin;
+    fin = fopen("arquivos/intro.txt", "r");
+    char ch;
+
+    //posiciionar cursos no caractere 8
+    fseek(fin, 7, SEEK_SET);
+    fscanf(fin, "%c", &ch);
+    fclose(fin);
+    printf("%c\n", ch);
+}
+
+void b(){
+    printf("\n\nb) Uma palavra \n");
+
+    FILE *fin;
+    fin = fopen("arquivos/intro.txt", "r");
+
+    //ler palavra formatada
+    char s[100];
+    fscanf(fin, "%s", s);
+    fclose(fin);
+    printf("%s\n", s);
+}
+
+void c(){
+    printf("\n\nc) A quinta palavra \n");
+
+    FILE *fin;
+    fin = fopen("arquivos/intro.txt", "r");
+    int contador = 0;
+    char s[100];
+    
+    //iterarpara ler quatro palavras
+    while(contador < 4){
+        if (fscanf(fin, "%99s",s) == 1){
+            contador +=1;
+        }
+    }
+
+    //ler e mostrar a quinta palavra
+    fscanf(fin, "%s", s);
+    fclose(fin);
+    printf("%s\n", s);
+}
+
+void d(){
+    printf("\n\nd) Uma linha \n");
+
+    FILE *fin;
+    fin = fopen("arquivos/intro.txt", "r");
+    char linha[200];
+
+    fgets(linha,sizeof(linha),fin);
+    printf("%s",linha);
+    fclose(fin);
+}
+
+int main(){
+    printf("Considerando o arquivo texto apresentado, o que o trecho de código abaixo faz?\nesse trecho lê o arquivo e exibe o primeiro código de linha");
+    a(); //Letra a
+    b(); //Letra b
+    c(); //Letra c
+    d(); //Letra d
+}
