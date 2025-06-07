56215da429373dd935ce01bb3629d9a1fe9dd83f
Joaof14 <joao-pedro032011@hotmail.com>
2025-06-07 14:42:54 -0300

    ""

--- Un1/Lab04/q2.c
+++ Un1/Lab04/q2.c
@@ -1,2 +1,22 @@
 /*Escreva um programa para ler todos os números do texto e gravá-los em outro
 arquivo.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    FILE * arquivo = fopen("arquivos/q2_ex.txt", "r");
+     if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+    int n;
+
+    while (fscanf(arquivo, "%d", &n) == 1){
+        printf("%d\n", n);
+    }
+    printf("%d", n);
+
+    fclose(arquivo);
+    return 1;
+
+}
